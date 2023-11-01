2294c7b540d33eb4312d2250e49e35917b5d7430
srinivasmane09 <manesrinivas11@gmail.com>
2023-11-01 14:39:23 -0500

    commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tigerRouter = require('./routes/tiger');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tiger', tigerRouter);
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
